6cbe536bff662c469ea7bfbb9332c68b0340d52d
JuanMiguelGin <d9exorz@gmail.com>
2026-05-15 14:14:32 +0200

    web

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,28 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const GA_ID = 'G-EEV6L8YMWR';
+
 export default function Document() {
   return (
     <Html lang="es">
       <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', { anonymize_ip: true });
+            `,
+          }}
+        />
+
+        {/* Fuentes */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
